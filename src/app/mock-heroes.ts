6809dc38adbccb84691e0cbe70ee8aebb5668778
Franck Monge <franckyroquai@gmail.com>
2023-07-01 19:00:07 +0200

    creation liste de heros

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'The Coon' },
+  { id: 13, name: 'Mysterio' },
+  { id: 14, name: 'Guingass Boumbass' },
+  { id: 15, name: 'Deadpool' },
+  { id: 16, name: 'Iron Man' },
+  { id: 17, name: 'Thor' },
+  { id: 18, name: 'Wonder Woman' },
+  { id: 19, name: 'Black Canary' },
+  { id: 20, name: 'Tornada Blanca' }
+];
